32c5979e945b5c9ae52d8e54b719bca398ba724c
akemalsaglam <alikemalsaglam90@gmail.com>
2022-03-20 21:53:18 +0300

    add extended scalars.

---
+++ src/main/java/com/aks/commons/graphql/GraphQLScalars.java
@@ -0,0 +1,56 @@
+package com.aks.commons.graphql;
+
+import graphql.scalars.ExtendedScalars;
+import graphql.schema.GraphQLScalarType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class GraphQLScalars {
+
+    @Bean
+    public GraphQLScalarType uuid() {
+        return ExtendedScalars.UUID;
+    }
+
+    @Bean
+    public GraphQLScalarType graphQLLong() {
+        return ExtendedScalars.GraphQLLong;
+    }
+
+    @Bean
+    public GraphQLScalarType graphQLBigInteger() {
+        return ExtendedScalars.GraphQLBigInteger;
+    }
+
+    @Bean
+    public GraphQLScalarType graphQLShort() {
+        return ExtendedScalars.GraphQLShort;
+    }
+
+    @Bean
+    public GraphQLScalarType date() {
+        return ExtendedScalars.Date;
+    }
+
+    @Bean
+    public GraphQLScalarType dateTime() {
+        return ExtendedScalars.DateTime;
+    }
+
+    @Bean
+    public GraphQLScalarType time() {
+        return ExtendedScalars.Time;
+    }
+
+    @Bean
+    public GraphQLScalarType url() {
+        return ExtendedScalars.Url;
+    }
+
+    @Bean
+    public GraphQLScalarType json() {
+        return ExtendedScalars.Json;
+    }
+}
