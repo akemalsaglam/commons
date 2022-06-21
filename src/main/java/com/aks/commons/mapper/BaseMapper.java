6df434acbfa20c290b84e9f2d568e7cd351e1527
akemalsaglam <alikemalsaglam90@gmail.com>
2022-06-22 00:05:59 +0300

    refactor.

--- src/main/java/com/aks/commons/mapper/BaseMapper.java
+++ src/main/java/com/aks/commons/mapper/BaseMapper.java
@@ -3,6 +3,7 @@ package com.aks.commons.mapper;
 
 import com.aks.commons.controller.BaseRequest;
 import com.aks.commons.controller.BaseResponse;
+import org.mapstruct.MappingTarget;
 
 import java.util.List;
 
@@ -13,6 +14,8 @@ public interface BaseMapper<Entity, Request extends BaseRequest, Response extend
     List<Response> mapEntityListToResponseList(List<Entity> entity);
 
     Entity mapRequestToEntity(Request request);
+
+    Entity mapRequestToEntity(Request request, @MappingTarget Entity entity);
 
     List<Entity> mapRequestListToEntitylist(List<Request> request);
 }
