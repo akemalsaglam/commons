76bdd88995c3dbe7d34280aaf4283e091b7ebbeb
akemalsaglam <alikemalsaglam90@gmail.com>
2022-07-06 22:47:59 +0300

    refactor access tokens.

--- src/main/java/com/aks/commons/security/accesstoken/impl/PasswordResetToken.java
+++ src/main/java/com/aks/commons/security/accesstoken/impl/PasswordResetToken.java
@@ -1,13 +1,8 @@
 package com.aks.commons.security.accesstoken.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
 public class PasswordResetToken extends BaseToken {
     public static final long TOKEN_VALIDITY_SECONDS = 2 * 60 * 60; //2 HOURS
 
-    @Autowired
     public PasswordResetToken() {
         super(TOKEN_VALIDITY_SECONDS);
     }
