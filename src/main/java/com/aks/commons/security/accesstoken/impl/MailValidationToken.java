f3241c623042133b45bfb617b1f980a1d5cceeea
akemalsaglam <alikemalsaglam90@gmail.com>
2022-07-06 22:54:41 +0300

    refactor.

--- src/main/java/com/aks/commons/security/accesstoken/impl/MailValidationToken.java
+++ src/main/java/com/aks/commons/security/accesstoken/impl/MailValidationToken.java
@@ -1,9 +1,14 @@
 package com.aks.commons.security.accesstoken.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MailValidationToken extends BaseToken {
 
     public static final long TOKEN_VALIDITY_SECONDS = 1 * 24 * 60 * 60; //1 DAY
 
+    @Autowired
     public MailValidationToken() {
         super(TOKEN_VALIDITY_SECONDS);
     }
