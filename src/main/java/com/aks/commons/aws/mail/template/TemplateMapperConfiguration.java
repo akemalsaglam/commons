544bb2f59b962efee518d93edd9dc04ff829ee04
akemalsaglam <alikemalsaglam90@gmail.com>
2022-07-06 21:56:05 +0300

    refactor template mapper configuration

--- src/main/java/com/aks/commons/aws/mail/template/TemplateMapperConfiguration.java
+++ src/main/java/com/aks/commons/aws/mail/template/TemplateMapperConfiguration.java
@@ -3,19 +3,15 @@ package com.aks.commons.aws.mail.template;
 import freemarker.cache.ClassTemplateLoader;
 import freemarker.cache.TemplateLoader;
 import freemarker.template.Configuration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
-@Component
 public class TemplateMapperConfiguration {
 
     public static final String MAIL_TEMPLATES_PATH = "/mail-templates";
 
-    @Bean
-    public FreeMarkerConfigurer freeMarkerConfigurer() {
+    public FreeMarkerConfigurer getConfigurer(String mailTemplatePath) {
         Configuration configuration = new Configuration(Configuration.VERSION_2_3_27);
-        TemplateLoader templateLoader = new ClassTemplateLoader(this.getClass(), MAIL_TEMPLATES_PATH);
+        TemplateLoader templateLoader = new ClassTemplateLoader(this.getClass(), mailTemplatePath);
         configuration.setTemplateLoader(templateLoader);
         FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
         freeMarkerConfigurer.setConfiguration(configuration);
