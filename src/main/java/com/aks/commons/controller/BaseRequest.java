ea3f5a6d55dd64766f1e3fddd44210e054352cb0
akemalsaglam <alikemalsaglam90@gmail.com>
2022-08-25 13:06:45 +0300

    refactor baseRequest

--- src/main/java/com/aks/commons/controller/BaseRequest.java
+++ src/main/java/com/aks/commons/controller/BaseRequest.java
@@ -3,12 +3,19 @@ package com.aks.commons.controller;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
 import java.io.Serializable;
+import java.time.ZonedDateTime;
 
 @Data
 @NoArgsConstructor
 public class BaseRequest implements Serializable {
-
     private Object id;
+    private @Email String createdByEmail;
+    private ZonedDateTime createdTime;
+    private @Email String updatedByEmail;
+    private ZonedDateTime updatedTime;
+    private @Email String deletedByEmail;
+    private ZonedDateTime deletedTime;
 
 }
