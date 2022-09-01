685eb01e0f10d07ef6d05383d0e2f58847169f10
akemalsaglam <alikemalsaglam90@gmail.com>
2022-09-01 15:40:22 +0300

    add audit fields into baseresponse.

--- src/main/java/com/aks/commons/controller/BaseResponse.java
+++ src/main/java/com/aks/commons/controller/BaseResponse.java
@@ -3,10 +3,17 @@ package com.aks.commons.controller;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
 import java.io.Serializable;
+import java.time.ZonedDateTime;
 
 @Data
 @NoArgsConstructor
 public class BaseResponse implements Serializable {
-
+    private @Email String createdByEmail;
+    private ZonedDateTime createdTime;
+    private @Email String updatedByEmail;
+    private ZonedDateTime updatedTime;
+    private @Email String deletedByEmail;
+    private ZonedDateTime deletedTime;
 }
