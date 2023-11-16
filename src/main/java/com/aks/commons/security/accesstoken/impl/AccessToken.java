bcbd24e7e0ff3fee145d8f37ac724b3febb2b2bf
akemalsaglam <alikemalsaglam90@gmail.com>
2023-11-16 21:35:16 +0100

    fix accesstoken error.

--- src/main/java/com/aks/commons/security/accesstoken/impl/AccessToken.java
+++ src/main/java/com/aks/commons/security/accesstoken/impl/AccessToken.java
@@ -1,5 +1,8 @@
 package com.aks.commons.security.accesstoken.impl;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class AccessToken extends BaseToken {
 
   public static final long TOKEN_VALIDITY_SECONDS = 30 * 24 * 60 * 60; //30 DAYs
